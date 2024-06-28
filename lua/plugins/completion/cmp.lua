3c82e463fd29bd1f8806dd4f60d3f5a246fb3788
lulu <lucas.verbeiren@gmail.com>
2024-06-28 15:15:15 +0200

    added noice.nvim + bordered cmp stuff

--- lua/plugins/completion/cmp.lua
+++ lua/plugins/completion/cmp.lua
@@ -9,7 +9,6 @@ return {
 			local luasnip = require("luasnip")
 
 			require("luasnip.loaders.from_vscode").lazy_load()
-			luasnip.config.setup({})
 
 			cmp.setup({
 				snippet = {
@@ -17,6 +16,16 @@ return {
 						luasnip.lsp_expand(args.body)
 					end,
 				},
+
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+
 				mapping = cmp.mapping.preset.insert({
 					["<C-n>"] = cmp.mapping.select_next_item(),
 					["<C-p>"] = cmp.mapping.select_prev_item(),
