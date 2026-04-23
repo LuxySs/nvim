509204c4e497105e7001e3a9eb506a3e6be4c337
lulu <lucas.verbeiren@gmail.com>
2026-04-23 15:57:20 +0200

    gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -26,7 +26,7 @@ return {
         end
 
         -- Navigation
-        map({ 'n', 'v' }, ']c', function()
+        map({ 'n', 'v' }, ']h', function()
           if vim.wo.diff then
             return ']c'
           end
@@ -36,7 +36,7 @@ return {
           return '<Ignore>'
         end, { expr = true, desc = 'Jump to next hunk' })
 
-        map({ 'n', 'v' }, '[c', function()
+        map({ 'n', 'v' }, '[h', function()
           if vim.wo.diff then
             return '[c'
           end
@@ -51,27 +51,37 @@ return {
         map('v', '<leader>hs', function()
           gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
         end, { desc = 'stage git hunk' })
+
         map('v', '<leader>hr', function()
           gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
         end, { desc = 'reset git hunk' })
+
         -- normal mode
-        map('n', '<leader>gs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>gr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>gS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>gu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>gR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>gp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>gb', function()
+        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git [s]tage hunk' })
+        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git [r]eset hunk' })
+        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git [S]tage buffer' })
+        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git [R]eset buffer' })
+        map('n', '<leader>hp', gs.preview_hunk, { desc = 'git [p]review hunk' })
+        map('n', '<leader>hi', gs.preview_hunk_inline, { desc = 'git preview hunk [i]nline' })
+
+        map('n', '<leader>hb', function()
           gs.blame_line({ full = false })
         end, { desc = 'git blame line' })
-        map('n', '<leader>gd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>gD', function()
-          gs.diffthis('~')
-        end, { desc = 'git diff against last commit' })
+
+        map('n', '<leader>hd', gs.diffthis, { desc = 'git [d]iff against index' })
+
+        map('n', '<leader>hD', function()
+          gs.diffthis('@')
+        end, { desc = 'git [D]iff against last commit' })
+
+        map('n', '<leader>hQ', function()
+          gs.setqflist('all')
+        end, { desc = 'git hunk [Q]uickfix list (all files in repo)' })
+        map('n', '<leader>hq', gs.setqflist, { desc = 'git hunk [q]uickfix list (all changes in this file)' })
 
         -- Toggles
-        map('n', '<leader>gtb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-        map('n', '<leader>gtd', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+        map('n', '<leader>tw', gs.toggle_word_diff, { desc = '[T]oggle git intra-line [w]ord diff' })
 
         -- Text object
         map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
