ccb01d0b065566e493bd5e10e60998620af26082
lulu <lucas.verbeiren@gmail.com>
2026-05-28 23:58:18 +0200

    better web-devicons dep management

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-web-devicons',
   auto_enable = true,
-  dep_of = { 'oil.nvim' },
+  dep_of = { 'oil.nvim', 'lualine.nvim', 'markview.nvim', 'snacks.nvim', 'which-key.nvim' },
   after = function(_)
     require('nvim-web-devicons').setup({})
   end,
--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -2,4 +2,11 @@ return {
   'markview.nvim',
   auto_enable = true,
   lazy = false,
+  after = function(_)
+    require('markview').setup({
+      preview = {
+        icon_provider = 'devicons', -- "internal", mini" or "devicons"
+      },
+    })
+  end,
 }
