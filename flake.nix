d6e5e0a51cd2cd6f12be9935c8b3f869a6ca748b
lulu <lucas.verbeiren@gmail.com>
2026-09-03 11:31:44 +0200

    remove comments

--- flake.nix
+++ flake.nix
@@ -5,20 +5,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     wrappers.url = "github:BirdeeHub/nix-wrapper-modules";
     wrappers.inputs.nixpkgs.follows = "nixpkgs";
-    # Demo on fetching plugins from outside nixpkgs
     plugins-lze = {
       url = "github:BirdeeHub/lze";
       flake = false;
     };
-    # These 2 are already in nixpkgs, however this ensures you always fetch the most up to date version!
     plugins-lzextras = {
       url = "github:BirdeeHub/lzextras";
       flake = false;
     };
+
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     plugins-rustaceanvim = {
       url = "github:mrcjkb/rustaceanvim";
       flake = false;
