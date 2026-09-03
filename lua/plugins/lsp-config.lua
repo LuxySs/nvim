2e45f375367d31863eb8705eceae282cbd0ec875
lulu <lucas.verbeiren@gmail.com>
2026-09-03 09:54:29 +0200

    wip

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,26 +10,28 @@ return {
   end,
   -- set up our on_attach function once before the spec loads
   before = function(_)
-    vim.lsp.config('*', {
-      on_attach = function(_, bufnr)
-        -- Create a command `:Format` local to the LSP buffer
-        vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-          vim.lsp.buf.format()
-        end, { desc = 'Format current buffer with LSP' })
-      end,
-    })
-
     vim.api.nvim_create_autocmd('LspAttach', {
       callback = function(event)
+        local bufnr = event.buf
+
         local map = function(keys, func, desc, mode)
+          if desc then
+            desc = 'LSP: ' .. desc
+          end
           mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
         end
 
+        vim.api.nvim_buf_create_user_command(bufnr, 'Format', function()
+          vim.lsp.buf.format()
+        end, { desc = 'Format current buffer with LSP' })
+
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client:supports_method('textDocument/inlayHint', event.buf) then
+
+        if client and client:supports_method('textDocument/inlayHint') then
           map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+            local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+            vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
           end, '[T]oggle Inlay [H]ints')
         end
       end,
